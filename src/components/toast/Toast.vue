28fcec81a532baaa0faa398a1a951b1faf3ccdcf
Eding Muhamad <masaruedo@yahoo.com>
2021-06-09 22:32:51 +0700

    Styling the component

--- src/components/toast/Toast.vue
+++ src/components/toast/Toast.vue
@@ -24,3 +24,69 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.toast {
+  width: 300px;
+  background: #ecfdf5;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  box-shadow: 1px 5px 10px -5px rgba(0, 0, 0, 0.2);
+  position: relative;
+}
+
+.toast::before {
+  content: "";
+  width: 4px;
+  height: 100%;
+  background: #34d399;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.toast-icon {
+  width: 16px;
+  height: 16px;
+  background: #34d399;
+  border-radius: 50%;
+  padding: 7px;
+}
+
+.toast-icon svg {
+  fill: #ecfdf5;
+}
+
+.toast-content {
+  flex-grow: 1;
+  margin: 0 1rem;
+}
+
+.toast-title {
+  font-weight: 700;
+  margin-bottom: 0.5rem;
+}
+
+.toast-message {
+  font-size: 14px;
+  color: #6b7280;
+}
+
+.toast-button {
+  width: 1.5em;
+  height: 1.5em;
+  border: none;
+  padding: 0;
+  color: #9ca3af;
+  opacity: 0.7;
+  background: transparent;
+  cursor: pointer;
+  font-size: 1.5em;
+}
+
+.toast-button:hover {
+  opacity: 1;
+}
+</style>
